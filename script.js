1fbda22b3c3545d5a3512321e291ee27a7aea41a
Zachary Levin <47122362+zlevin1228@users.noreply.github.com>
2024-02-23 15:42:08 +0000

    added win conditions

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function playGame() {
   ) {
     // Asks for user choices and if the words uppercase match then it will not give an error message
     if (userChoice.toUpperCase() === "ROCK") {
-        (document.getElementById("usersChoice").src = "Images/rockimage.jpg");
+      (document.getElementById("usersChoice").src = "Images/rockimage.jpg");
     } else if (userChoice.toUpperCase() === "PAPER") {
-        (document.getElementById("usersChoice").src = "Images/paper.jpg");
+      (document.getElementById("usersChoice").src = "Images/paper.jpg");
     } else if (userChoice.toUpperCase() === "SCISSORS") {
-        (document.getElementById("usersChoice").src = "Images/scissors.webp");
+      (document.getElementById("usersChoice").src = "Images/scissors.webp");
     } else if (userChoice.toUpperCase() === "BOMB") {
-        (document.getElementById("usersChoice").src = "Images/bomb.jpg");
+      (document.getElementById("usersChoice").src = "Images/bomb.jpg");
     }
   } else {
     alert("Error!");
@@ -39,4 +39,36 @@ function playGame() {
     computerChoice = "scissors";
     (document.getElementById("computersChoice").src = "Images/scissors.webp");
   }
+
+  var choice1 = userChoice; // sets new variables for the function
+  var choice2 = computerChoice;
+
+
+  if (choice1.toUpperCase() === choice2.toUpperCase()) {
+    document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">It is a tie!</h3>';
+  } else if (choice1.toUpperCase() === "ROCK") {
+    if (choice2.toUpperCase() === "PAPER") {
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose PAPER, you LOSE!</h3>';
+    } else
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose SCISSORS, you WIN!</h3>';
+  } else if (choice1.toUpperCase() === "PAPER") {
+    if (choice2.toUpperCase() === "SCISSORS") {
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose SCISSORS, you LOSE!</h3>';
+    } else
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose ROCK, you WIN!</h3>';
+  } else if (choice1.toUpperCase() === "SCISSORS") {
+    if (choice2.toUpperCase() === "ROCK") {
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose ROCK, you lose!</h3>';
+    } else
+      document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">Computer chose PAPER, you WIN!</h3>';
+  } else if (choice1.toUpperCase() === "BOMB") {
+    document.getElementById('messageResult').innerHTML = '<h3 class="align-items-center">You found the secret message! You win!</h3>'
+  }
+
+
 }
+
+
+
+
+
